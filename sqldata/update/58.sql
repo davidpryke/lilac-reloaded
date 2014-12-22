f7c862a75abc0f0c28b91a0b4a7c6d5dd63f6af1
Rene Hadler <rene.hadler@iteas.at>
2014-12-22 09:46:00 +0100

    When updating and introducing the new transparency fields set the default to 255,255,255 else it can be strange behavior because the default were 0,0,0 and statusmap will not display correctly

--- sqldata/update/58.sql
+++ sqldata/update/58.sql
@@ -5,6 +5,8 @@ ALTER TABLE `nagios_cgi_configuration`
   ADD `color_transparency_index_b` INTEGER,
   ADD `result_limit` INTEGER;
 
+UPDATE `nagios_cgi_configuration` SET color_transparency_index_r=255, color_transparency_index_g=255, color_transparency_index_b=255;
+
 REPLACE INTO `label`(section, name, label)  VALUES('nagios_cgi_desc', 'authorized_for_read_only', 'A comma-delimited list of usernames that have read-only rights in the CGIs. This will block any service or host commands normally shown on the extinfo CGI pages. It will also block comments from being shown to read-only users.');
 REPLACE INTO `label`(section, name, label)  VALUES('nagios_cgi_desc', 'color_transparency_index', 'These options set the r,g,b values of the background color used the statusmap CGI, so normal browsers that can''t show real png transparency set the desired color as a background color instead (to make it look pretty). Defaults to white: (R,G,B) = (255,255,255).');
 REPLACE INTO `label`(section, name, label)  VALUES('nagios_cgi_desc', 'result_limit', 'The number of services being shown in Nagios at the same time.');
