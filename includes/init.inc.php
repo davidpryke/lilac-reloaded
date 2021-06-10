e2b531c79ef70b580c627363bafdd176e98fd230
David J. Pryke <davidpryke@users.noreply.github.com>
2021-06-10 09:18:42 -0400

    Update init.inc.php

--- includes/init.inc.php
+++ includes/init.inc.php
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 define("LILAC_VERSION", "2.0.8");
-define("LILAC_VERSION_BUILD", "59");
+define("LILAC_VERSION_BUILD", "60");
 define("LILAC_NAME", "lilac-reloaded");
 define("LILAC_FS_ROOT", dirname(__FILE__) . "/../");
 
